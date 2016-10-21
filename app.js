3b982a5f9bb65ea9c2f064d61337bb9e933e489a
Dion <Dion.Fulwood@gmail.com>
2016-10-20 22:27:00 -0700

    Adds bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var routes = require('./routes');
+var bodyParser = require('body-parser');
+var routes = require('./api/routes');
 
 app.set("port", 3000);
 
 app.use(express.static(path.join(__dirname, "public")));
+
+//Add bodyParser middleware to deal with any forms to be posted
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/api', routes);
 
