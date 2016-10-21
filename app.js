a7e0d9093eb03657d03816c8ffc64ac7dd1b1fbf
Dion <Dion.Fulwood@gmail.com>
2016-10-20 19:08:32 -0700

    Adds Bootstrap and JQuery. Links css to index.html

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set("port", 3000);
 
-app.get('/', function (req, res) {
-  console.log("GET the homepage");
-  res.send("Express yourself")
-});
+app.use(express.static(path.join(__dirname, "public")));
+
+
 
 var server = app.listen(app.get('port'), function () {
   var port = server.address().port;
