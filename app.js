b883d6960ff1079b67b548794bc543debbcd1a10
Dion <Dion.Fulwood@gmail.com>
2016-10-20 18:37:35 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set("port", 3000);
+
+app.get('/', function (req, res) {
+  console.log("GET the homepage");
+  res.send("Express yourself")
+});
+
+var server = app.listen(app.get('port'), function () {
+  var port = server.address().port;
+  console.log("Magic happens on port" + port);
+});
